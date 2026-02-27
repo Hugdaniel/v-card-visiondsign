4b1f602c5b25be6870ef4dc001067c57295edd2a
Daniel Barrios <85hbdaniel@gmail.com>
2026-02-27 20:44:13 -0300

    vcard terminada

--- v-card/js/config.js
+++ v-card/js/config.js
@@ -14,5 +14,6 @@ const CONFIG_VCARD = {
     mensajeWpp: "¡Hola! Vi tu V-Card y quiero consultar por un presupuesto.",
     
     // Colores (Para que el JS los aplique si querés ser muy pro)
-    temaColor: " background: #6025f5; background: -webkit-linear-gradient(to right, #ff5555, #6025f5);background: linear-gradient(to right, #ff5555, #6025f5);"
+    temaColor: " background: -webkit-linear-gradient(135deg, #131067,#000e57);"
 };
+
